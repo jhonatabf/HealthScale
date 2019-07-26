68994f373a069c95755498b65b0699a746d03d22
Jhonata Rebouças <jhonata.reboucas@hospitalsaodomingos.com.br>
2019-07-26 08:58:37 -0300

    Criação do widget filho

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Health Scale",
-    home: Container(color: Colors.black,)
+    home: Container(color: Colors.grey[50],
+          child: Container (
+            margin: const EdgeInsets.all(10.0),
+            color: Colors.lightBlue[300],
+            width: 30,
+            height: 12,
+            ),)
   ));
 }
