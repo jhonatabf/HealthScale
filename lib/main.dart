4a3f90b30f8de43fdc83b3da6036ea7784bfed64
Jhonata Rebouças <jhonata.reboucas@hospitalsaodomingos.com.br>
2019-07-29 13:45:12 -0300

    Alteração do layout para aproximação do adobe reader

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text("Health Scale",
           style: TextStyle(
-              color: Colors.cyan[400],),),
+              color: Colors.grey[700],),),
         backgroundColor: Colors.white,
         //actions: <Widget>[
         //  IconButton(icon: Icon(Icons.home, color: Colors.grey[400],), onPressed: () {},)
@@ -52,32 +52,30 @@ Widget base() {
           Container(
             alignment: Alignment.topLeft,
             padding: const EdgeInsets.fromLTRB(14, 20, 12, 5),
-            child: Text("Escalas Disponíveis",
+            child: Text("ESCALAS DISPONÍVEIS",
             style: TextStyle(color: Colors.grey[600]),),
+            decoration: const BoxDecoration(
+            border: Border(
+              bottom: BorderSide(width: 1.0, color: Color(0xFFFFDDDDDD),),
+            ),),
           ),
           Container(
-            margin: const EdgeInsets.fromLTRB(14, 0, 10, 5),
-            height: 50,
-            color: Colors.cyan[200],
+            decoration: const BoxDecoration(
+            border: Border(
+              bottom: BorderSide(width: 1.0, color: Color(0xFFFFDDDDDD),),
+            ),),
+            margin: const EdgeInsets.fromLTRB(0, 0, 0, 5),
+            height: 60,
             child: const Center(child: Text('Escala Braden')),
           ),
           Container(
-            margin: const EdgeInsets.fromLTRB(14, 0, 10, 5),
-            height: 50,
-            color: Colors.cyan[200],
-            child: const Center(child: Text('Escala TEV')),
-          ),
-          Container(
-            margin: const EdgeInsets.fromLTRB(14, 0, 10, 5),
-            height: 50,
-            color: Colors.cyan[200],
-            child: const Center(child: Text('Entry MEWS')),
-          ),
-          Container(
-            margin: const EdgeInsets.fromLTRB(14, 0, 10, 5),
-            height: 50,
-            color: Colors.cyan[200],
-            child: const Center(child: Text('Entry NRS-2002')),
+            decoration: const BoxDecoration(
+            border: Border(
+              bottom: BorderSide(width: 1.0, color: Color(0xFFFFDDDDDD),),
+            ),),
+            margin: const EdgeInsets.fromLTRB(0, 0, 0, 5),
+            height: 60,
+            child: const Center(child: Text('Escala MEWS')),
           ),
         ],
 
