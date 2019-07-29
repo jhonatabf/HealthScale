6a7a67cf7d9433fab9d46e1d19e3e9186600b227
Jhonata Rebouças <jhonata.reboucas@hospitalsaodomingos.com.br>
2019-07-29 10:37:55 -0300

    Criação do Layout com implementação da listagem de escalas e títulos

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]);
+
   runApp(MaterialApp(
     title: "Health Scale",
     home: Home(),
@@ -21,17 +28,60 @@ class _HomeState extends State<Home> {
           style: TextStyle(
               color: Colors.cyan[400],),),
         backgroundColor: Colors.white,
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.home, color: Colors.grey[400],), onPressed: () {},)
-        ],),
+        //actions: <Widget>[
+        //  IconButton(icon: Icon(Icons.home, color: Colors.grey[400],), onPressed: () {},)
+        //],
+        ),
         backgroundColor: Colors.grey[100],
-        body: Column(children: <Widget>[
-          Padding(padding: EdgeInsets.all(8.0),),
-          Text("Escala MEWS", 
-            style: TextStyle(
-              color: Colors.grey[800],),),
-        ],),
+        body: base(),
     );
     return scaffold;
   }
+Widget base() {
+      return Column(
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+            padding: const EdgeInsets.all(10),
+            color: Colors.green[500],
+            child: const Center(
+                child: Text('Estamos tentando manter o aplicativo sem propaganda ou qualquer outro tipo de distração, o seu foco é a saúde então pedimos a sua ajuda para mante-lo assim e melhorar a aplicação ainda mais.',
+                style: TextStyle(color: Colors.white), textAlign: TextAlign.justify,),
+                ),
+          ),
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.fromLTRB(14, 20, 12, 5),
+            child: Text("Escalas Disponíveis",
+            style: TextStyle(color: Colors.grey[600]),),
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(14, 0, 10, 5),
+            height: 50,
+            color: Colors.cyan[200],
+            child: const Center(child: Text('Escala Braden')),
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(14, 0, 10, 5),
+            height: 50,
+            color: Colors.cyan[200],
+            child: const Center(child: Text('Escala TEV')),
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(14, 0, 10, 5),
+            height: 50,
+            color: Colors.cyan[200],
+            child: const Center(child: Text('Entry MEWS')),
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(14, 0, 10, 5),
+            height: 50,
+            color: Colors.cyan[200],
+            child: const Center(child: Text('Entry NRS-2002')),
+          ),
+        ],
+
+      );
+    }
+
 }
